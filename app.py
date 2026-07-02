88479b4af7f0e5adfb081de0a09ea4381339022e
pramesthiananta <pramesthiananta@gmail.com>
2026-07-02 15:50:31 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from ultralytics import YOLO
+
+st.set_page_config(
+    page_title="Deteksi Cacat Radiografi",
+    page_icon="🩻",
+    layout="wide"
+)
+
+st.title("🩻 Deteksi Cacat Citra Radiografi Pengelasan")
+
+st.write("Model berhasil dimuat.")
+
+@st.cache_resource
+def load_model():
+    return YOLO("weights/best.pt")
+
+model = load_model()
+
+st.success("✅ Model YOLO berhasil dimuat.")
